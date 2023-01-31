e32a1d350493172aba2e0baf48566de9fea0fd2a
Sergi Juanati <wint3rmute@gmail.com>
2023-01-31 17:15:25 +0100

    Fixed ESM setup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-console.log('hi there');
+import fetch, { Headers } from 'node-fetch';
 
+(
+    async () => {
+        try {
+            // Add the API key to an header object
+            const meta = {
+                "x-dune-api-key": "YOUR_API_KEY"
+            };
+            const header = new Headers(meta);
+
+            //  Call the Dune API
+            const response = await fetch('https://api.dune.com/api/v1/query/1258228/execute', {
+                method: 'POST',
+                headers: header
+            });
+            const body = await response.text();
+
+            // Log the returned response
+            console.log(body);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+)();
